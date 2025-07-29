0b8f6c9b69b09f42cce37f519e68fbc13c536d8a
Lucas Guido <lucasguidocp@gmail.com>
2025-07-29 20:42:19 -0300

    Insertion working

--- src/bptree_interface.cpp
+++ src/bptree_interface.cpp
@@ -24,7 +24,7 @@ BPTreeInterface::BPTreeInterface()
       bt(btree<Pair, BTREE_ORDER>(pm)),
       record_manager("music.bin", false)
 {
-    curr_page_id = 0;
+    curr_page_id = 1;
 }
 
 BPTreeInterface::~BPTreeInterface() { }
@@ -32,8 +32,6 @@ BPTreeInterface::~BPTreeInterface() { }
 void BPTreeInterface::insert(string &insert_path) {
     set<PieceInfo> album_pieces;
     read_album_tags(fs::path(insert_path), album_pieces);
-
-    cout << "oi" << endl;
 
     for (PieceInfo p : album_pieces) {
         Pair new_node;
@@ -43,10 +41,17 @@ void BPTreeInterface::insert(string &insert_path) {
             new_node.id[i] = p.composer[i];
         }
         for (; (i - p.composer.length()) < p.catalog.length() && i < ID_SIZE - 1; i++) {
-            new_node.id[i] = p.catalog[i];
+            new_node.id[i] = p.catalog[i - p.composer.length()];
         }
         new_node.id[i] = '\0';
 
+        cout << new_node.id << endl; 
+
+        new_node.page_id = curr_page_id;
+
+        record_manager.save(curr_page_id, p);
         bt.insert(new_node);
+
+        curr_page_id++;
     }
 }
